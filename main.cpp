6c096fa96a3547942b100963283958bb8cbaf199
mdepp <mdepp@ualberta.ca>
2017-10-07 22:25:23 -0600

    Updated and tested Perlin noise implementation

---
+++ main.cpp
@@ -0,0 +1,66 @@
+#include <SDL2/SDL.h>
+
+#include "perlin.h"
+#include <algorithm>
+#include <glm/glm.hpp>
+#include <vector>
+
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+    const int imageSize = 800;
+    const int nCells = 10;
+    const float increment = imageSize / (float)nCells;
+    PerlinNoise pn(nCells, nCells);
+
+    // A quick hack to get noise displaying
+    vector<char> pixels;
+    for(int i=0; i<imageSize; ++i)
+    {
+        for (int j=0; j<imageSize; ++j)
+        {
+            auto x = i/(double)imageSize*nCells;
+            auto y = j/(double)imageSize*nCells;
+			auto val = pn.getPointValue({ x, y });
+			val = std::max(0.0, std::min(1.0, val));
+			pixels.push_back(0x0000FF * val);
+        }
+    }
+
+    SDL_Init(SDL_INIT_VIDEO);
+    auto window = SDL_CreateWindow("Perlin noise",
+        SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
+        imageSize, imageSize, 0);
+    
+    auto noise_surface = SDL_CreateRGBSurfaceFrom(
+        (void*)pixels.data(),
+        imageSize, imageSize,
+        24,
+        imageSize,
+        0xFF0000,
+        0x00FF00,
+        0x0000FF,
+        0);
+
+    auto screen = SDL_GetWindowSurface(window);
+
+	SDL_Event e;
+	bool running = true;
+	while (running)
+	{
+		while (SDL_PollEvent(&e))
+		{
+			if (e.type == SDL_QUIT) running = false;
+			if (e.type == SDL_KEYUP && e.key.keysym.sym == SDLK_ESCAPE) running = false;
+		}
+
+		SDL_BlitSurface(noise_surface, nullptr, screen, nullptr);
+		SDL_UpdateWindowSurface(window);
+	}
+
+    SDL_FreeSurface(noise_surface);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+    return 0;
+}
--- perlin.h
+++ perlin.h
@@ -1,4 +1,9 @@
-// Based off of http://flafla2.github.io/2014/08/09/perlinnoise.html
+/*
+ * This algorithm is loosely based off of a few sources (listed below). While it
+ * may be less efficient, I feel it is slightly more readable.
+ *  - http://flafla2.github.io/2014/08/09/perlinnoise.html
+ *  - https://en.wikipedia.org/wiki/Perlin_noise
+ */
 
 /*
  THE PERLIN NOISE ALGORITHM
@@ -9,7 +14,10 @@
  a value for each corner. Average these values to get final value at that point.
  */
 
+#include <algorithm>
+#include <assert.h>
 #include <cmath>
+#include <cstdlib>
 #include <vector>
 #include <glm/glm.hpp>
 
@@ -17,42 +25,64 @@ class PerlinNoise
 {
 public:
     PerlinNoise(int width, int height)
-        : m_width(width), m_height(height)
+        : m_width(width), m_height(height),
+		m_gradientChoices({ glm::vec2(1, 1), glm::vec2(1, -1), glm::vec2(-1, 1), glm::vec2(-1, -1) })
     {
-        m_gradientChoices{{1,1},{-1,1},{1,-1},{-1,-1}};
-        
-        gradients.resize(width+1);
-        for(auto& rows : gradients)
+        m_gradients.resize(width);
+        for(auto& rows : m_gradients)
         {
-            rows.resize(height+1);
+            rows.resize(height);
             for (auto&  corner : rows)
             {
-                corner = rand() % m_gradientChoices.length(); // TODO: Use better random generator
+                corner = std::rand() % m_gradientChoices.size(); // TODO: Use better random generator
             }
         }
     }
     
-private:
     double getPointValue(glm::vec2 pos)
     {
-        // Calculate enclosing integer bounds
-        auto lowerBound = glm::vec2(std::floor(pos.x)), std::floor(pos.y));
-        auto upperBound = glm::vec2(std::ceil(pos.x)), std::ceil(pos.y));
-        // Calculate position of all corners
-        std::vector<glm::vec2> corners = {
-            lowerBound,
-            {lowerBound.x, upperBound.y},
-            upperBound,
-            {upperBound.x, lowerBound.y}
-        };
-        for (auto corner : corners)
-        {
-            auto distance = pos - corner;
-            auto gradient = getGradient(corner);
-            auto influence = glm::dot(distance, gradient);
-            // Fade b/w influence for each corner...
-        }
+		auto lower_bound = glm::ivec2(std::floor(pos.x), std::floor(pos.y));
+		auto upper_bound = glm::ivec2(lower_bound.x + 1, lower_bound.y + 1);
 
+		auto weight = pos - glm::vec2(lower_bound);
+
+		float influence_low_left = getInfluence({ lower_bound.x, lower_bound.y }, pos);
+		float influence_low_right = getInfluence({ upper_bound.x, lower_bound.y }, pos);
+		float influence_high_left = getInfluence({ lower_bound.x, upper_bound.y }, pos);
+		float influence_high_right = getInfluence({ upper_bound.x, upper_bound.y }, pos);
+
+		float influence_low = interpolate(influence_low_left, influence_low_right, weight.x);
+		float influence_high = interpolate(influence_high_left, influence_high_right, weight.x);
+		float influence = interpolate(influence_low, influence_high, weight.y);
+		return (1.0+influence)/2.0; // Scale to [0, 1] for convenience
+	}
+
+private:
+
+    float ease(float t)
+    {
+		//auto val = (t*t*t)*(6.f*(t*t*t) - 15.f*t + 10.f);
+		auto val = t*t*(3 - 2 * t);
+		assert(val < 1.f);
+		return val;
+    }
+	float interpolate(float first, float second, float amount)
+	{
+		return first + ease(amount)*(second - first);
+	}
+	
+	float getInfluence(glm::ivec2 corner, glm::vec2 pos)
+	{
+		auto distance = pos - glm::vec2(corner);
+		auto gradient = getGradient(corner);
+		return glm::dot(distance, gradient);
+	}
+
+    glm::vec2 getGradient(glm::ivec2 corner)
+    {
+		auto x = (corner.x%m_width + m_width) % m_width; // Wrap greater/lesser values to [0, width-1]
+		auto y = (corner.y%m_height + m_height) % m_height;
+		return m_gradientChoices[m_gradients[x][y]];
     }
 
     const int m_width, m_height;
@@ -60,4 +90,4 @@ private:
     std::vector<glm::vec2> m_gradientChoices;
     std::vector<std::vector<int>> m_gradients; // Stores an index into gradient choices
 
-}
+};
